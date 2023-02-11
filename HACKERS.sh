64af8bd3b18e2b316cca0c4bfb2ae816ded3189e
Ibrahim Abed <92536829+messi7775@users.noreply.github.com>
2023-02-11 23:11:17 +0300

    Add files via upload

---
+++ HACKERS.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+C="\e[1;36m"
+R="\e[1;31m"
+E="\e[0m"
+G='\033[1;32m'
+echo -e "$C
+               ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+               ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+               ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  
+               ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  
+               ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+                ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+$E"
+echo -e "$R                               Coded By$G Zaeem Technical     $E"
+echo -e "$C                            Website:- $R https://www.facebook.com/messi77775  $E"
+figlet -f big Installing | lolcat
+apt update && apt upgrade
+apt install curl -y
+apt install wget -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install bash -y
+apt install cowsay -y
+apt install git -y
+apt install dart -y
+apt install toilet -y
+apt install fish -y
+apt install figlet -y
+apt install php -y
+apt install php -y
+apt install ruby -y
+apt install perl -y
+apt install clang -y
+apt install macchanger -y
+apt install jq -y
+apt install tor -
+apt install openssh -y
+apt install openssly unzip zip nodejs libxml2-utils grep bc ffmpeg zsh screenfetch
+apt-get update -y
+figlet -f big Done!!! | lolcat
+figlet -f standard Thank for using... | lolcat
